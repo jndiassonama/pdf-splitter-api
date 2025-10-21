acee6094cc0ab75ae895eb8aab3973283187c245
diassonama.nkonko <jndiassonama@hotmail.com>
2025-10-21 11:37:25 +0100

    Setup inicial do backend Spring Boot

---
+++ Backend/src/main/java/com/java/pdfsplitter/PdfsplitterApplication.java
@@ -0,0 +1,13 @@
+package com.java.pdfsplitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfsplitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfsplitterApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/java/pdfsplitter/PdfsplitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.pdfsplitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfsplitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
